ce1066e8d42bad8245bf9a427749098131eb22a5
Michał Sieczkowski <michal@ethworks.io>
2020-04-14 18:28:48 +0200

    Copy merkle-patricia-tree v3.0.0 files to new trie package directory

---
+++ packages/trie/src/secure.js
@@ -0,0 +1,15 @@
+const CheckpointTrie = require('./index')
+const secureInterface = require('./secure-interface')
+
+/**
+ * You can create a secure Trie where the keys are automatically hashed using **SHA3** by using `require('merkle-patricia-tree/secure')`. It has the same methods and constructor as `Trie`.
+ * @class SecureTrie
+ * @extends Trie
+ * @public
+ */
+module.exports = class SecureTrie extends CheckpointTrie {
+  constructor (...args) {
+    super(...args)
+    secureInterface(this)
+  }
+}
